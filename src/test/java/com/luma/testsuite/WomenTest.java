5d7ef7ce18598432103f65b1057642caeeb20295
shweta158dave <shweta158dave@gmail.com>
2023-05-09 14:15:36 +0100

    My Homework week 14

---
+++ src/test/java/com/luma/testsuite/WomenTest.java
@@ -0,0 +1,43 @@
+package com.luma.testsuite;
+
+import com.luma.customlisteners.CustomListeners;
+import com.luma.pages.HomePage;
+import com.luma.pages.MensPantPage;
+import com.luma.pages.OverNightDufflePage;
+import com.luma.pages.ShoppingCartPage;
+import com.luma.testbase.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(CustomListeners.class)
+public class WomenTest extends BaseTest {
+    HomePage homePage;
+    MensPantPage mensPantPage;
+    ShoppingCartPage shoppingCartPage;
+    OverNightDufflePage overNightDufflePage;
+    @BeforeMethod(alwaysRun = true)
+    public void inIt() {
+        shoppingCartPage = new ShoppingCartPage();
+        overNightDufflePage = new OverNightDufflePage();
+        homePage = new HomePage();
+        mensPantPage = new MensPantPage();
+    }
+    @Test(groups = {"sanity", "regression"})
+    public void userShouldAddProductSuccessFullyToShoppingCart() throws InterruptedException {
+        Thread.sleep(1000);
+        homePage.mouseHoverToMenMenu();
+        homePage.mouseHoverToBottomMenu();
+        homePage.mouseHoverAndClickOnPantMenu();
+        mensPantPage.mouseHoverAndClickOnPantSize();
+        mensPantPage.mouseHoverAndClickOnBlackColour();
+        mensPantPage.mouseHoverAndClickOnAddToCart();
+        Assert.assertEquals(mensPantPage.verifyProductAddedToCartSuccessfully(), "You added Cronus Yoga Pant to your shopping cart.");
+        mensPantPage.clickOnShoppingCart();
+        Assert.assertEquals(shoppingCartPage.verifyTextShoppingCart(), "Shopping Cart");
+        Assert.assertEquals(shoppingCartPage.verifyProductName(), "Cronus Yoga Pant");
+        Assert.assertEquals(shoppingCartPage.verifyProductSize(), "32");
+        Assert.assertEquals(shoppingCartPage.verifyProductColour(), "Black");
+    }
+}
